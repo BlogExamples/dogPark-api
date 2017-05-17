904c01be1419e53ce0de2f55c3abccaf79860767
Josh Kuehn <joshmkuehn@gmail.com>
2017-05-17 17:06:01 -0500

    Added versioning to the API

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
+
+  namespace :api, defaults: { format: :json } do
+
+    scope module: :v1, constraints: ApiConstraints.new(version:1 ) do
+      resources :dog_parks
+    end
+
+    scope module: :v2, constraints: ApiConstraints.new(version: 2, default: true) do
+      resources :dog_parks
+    end
+
+  end
+
   resources :dog_parks
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
