838d506fd6eae1903608eb23a0cd8e3884475ebf
Josh Kuehn <joshmkuehn@gmail.com>
2017-05-17 22:25:16 -0500

    Added Basic API Versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+
+    namespace :v1 do
+        resources :dog_parks
+    end
+
+  end
+
   resources :dog_parks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
